9234729c476b3480b1f0d91bb6af58829379d1a3
Kethan Vegunta <kethan@vegunta.com>
2022-03-23 22:46:18 -0700

    hash passwords; refine api; update ui

--- app.py
+++ app.py
@@ -2,16 +2,26 @@ import datetime
 import base64
 import os
 
-from flask import (Flask, render_template, redirect, flash, abort, request,
-                   Markup, session, jsonify)
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    flash,
+    abort,
+    request,
+    Markup,
+    session,
+    jsonify,
+)
 from flask_pymongo import PyMongo
 from flask_compress import Compress
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
 from bson.objectid import ObjectId
-import pyimgur
 from dotenv import load_dotenv
 from urllib.parse import quote
+import pyimgur
+from flask_bcrypt import Bcrypt
 
 import http_response_codes as status
 
@@ -20,13 +30,15 @@ app.url_map.strict_slashes = False
 
 if "DYNO" not in os.environ:
     load_dotenv()
+
 app.config.update(
     IMGUR_ID=os.environ["IMGUR_ID"],
     MONGO_URI=os.environ["MONGO_URI"],
-    SECRET_KEY=os.environ["SECRET_KEY"]
+    SECRET_KEY=os.environ["SECRET_KEY"],
 )
 app.config["ImgurObject"] = pyimgur.Imgur(app.config["IMGUR_ID"])
 mongo = PyMongo(app)
+flask_bcrypt = Bcrypt(app)
 Compress(app)
 cors = CORS(app, resources={"/api/*": {"origins": "*"}})
 
@@ -44,14 +56,21 @@ def logged_in(session):
 
 @app.route("/")
 def blogs():
-    return render_template("blogs.html", login_status=((session["logged_in"] if logged_in(session) else None)))
+    return render_template(
+        "blogs.html",
+        login_status=((session["logged_in"] if logged_in(session) else None)),
+    )
 
 
 @app.route("/post_blog")
 def post_blog():
     if logged_in(session):
         return render_template("post_blog.html", login_status=session["logged_in"])
-    flash(Markup("""Please <a style="text-decoration: underline;" href="/login">Login</a> or <a style="text-decoration: underline;" href="/sign_up">Sign Up</a> to Post a Blog"""))
+    flash(
+        Markup(
+            """Please <a style="text-decoration: underline;" href="/login">Login</a> or <a style="text-decoration: underline;" href="/sign_up">Sign Up</a> to Post a Blog"""
+        )
+    )
     return redirect("/")
 
 
@@ -61,17 +80,11 @@ def return_blog(page):
     if results is None:
         abort(404)
     elif "logged_in" not in session or session["logged_in"] is None:
-        return render_template(
-            "blog_template.html",
-            results=results,
-            login_status=None
-        )
+        return render_template("blog_template.html", results=results, login_status=None)
     else:
         results["text"] = Markup(results["text"])
         return render_template(
-            "blog_template.html",
-            results=results,
-            login_status=session["logged_in"]
+            "blog_template.html", results=results, login_status=session["logged_in"]
         )
 
 
@@ -82,13 +95,11 @@ def return_use(user):
         return render_template(
             "user_template.html",
             results_from_user=results,
-            login_status=session["logged_in"]
+            login_status=session["logged_in"],
         )
     else:
         return render_template(
-            "user_template.html",
-            results_from_user=results,
-            login_status=None
+            "user_template.html", results_from_user=results, login_status=None
         )
 
 
@@ -106,14 +117,16 @@ def sign_up():
                 "last_name": request.form.get("last_name"),
                 "username": request.form.get("username"),
                 "email": request.form.get("email").lower(),
-                "password": request.form.get("password")
+                "password": flask_bcrypt.generate_password_hash(
+                    request.form.get("password")
+                ).decode(),
             }
             if mongo.db.users.find_one({"email": doc["email"]}) is None:
                 session["logged_in"] = {
                     "first_name": doc["first_name"],
                     "last_name": doc["last_name"],
                     "email": doc["email"],
-                    "username": doc["username"]
+                    "username": doc["username"],
                 }
 
                 mongo.db.users.insert_one(doc)
@@ -139,18 +152,15 @@ def login():
     elif request.method == "POST":
         doc = {
             "email": request.form.get("email").lower(),
-            "password": request.form.get("password")
+            "password": request.form.get("password"),
         }
-        found = mongo.db.users.find_one({
-            "email": doc["email"],
-            "password": doc["password"]}
-        )
-        if found is not None:
+        found = mongo.db.users.find_one({"email": doc["email"]})
+        if flask_bcrypt.check_password_hash(found["password"], doc["password"]):
             session["logged_in"] = {
                 "first_name": found["first_name"],
                 "last_name": found["last_name"],
                 "email": found["email"],
-                "username": found["username"]
+                "username": found["username"],
             }
             flash("Successfully Logged In")
             return redirect("/")
@@ -173,32 +183,34 @@ def logout():
 @app.route("/api/v1/blogs")
 def api_blogs():
     to_return = []
-    for blog in reversed(sorted(list(mongo.db.blogs.find({})), key=lambda date: datetime.datetime.strptime(
-        date["date_released"] + date["time_released"], "%m/%d/20%y%H:%M:%S:%f"
-    ))):
+    for blog in sorted(
+        list(mongo.db.blogs.find({})),
+        key=lambda date: datetime.datetime.strptime(
+            date["date_released"] + date["time_released"], "%m/%d/20%y%H:%M:%S:%f"
+        ),
+        reverse=True,
+    ):
         blog["_id"] = str(blog["_id"])
         blog["link"] = f"https://blogger-101.herokuapp.com/{blog['link']}"
         to_return.append(blog)
     return jsonify(to_return)
 
 
-@app.route("/api/v1/add_blog_new", methods=["POST"])
-def add_blog_new():
+@app.route("/api/v1/post-blog", methods=["POST"])
+def api_post_blog():
     title = request.form.get("blog_title")
     name = ("_".join(title.split(" "))).lower()
     to_upload_image = app.config["ImgurObject"]._send_request(
         "https://api/v1.imgur.com/3/image",
         method="POST",
-        params={
-            "image": base64.b64encode(request.files["file"].read())
-        }
+        params={"image": base64.b64encode(request.files["file"].read())},
     )
     doc = {
         "title": title,
         "user": request.form.get("user"),
         "name": f"{name}.html",
         "text": request.form.get("blog_content"),
-        "link": "/blog/%s" % name,
+        "link": f"/blog/{name}",
         "date_released": datetime.datetime.utcnow().strftime("%m/%d/%Y"),
         "time_released": datetime.datetime.utcnow().strftime("%H:%M:%S:%f"),
         "comments": [],
@@ -209,7 +221,7 @@ def add_blog_new():
     return redirect("/")
 
 
-@app.route("/api/v1/check_user", methods=["POST"])
+@app.route("/api/v1/check-user", methods=["POST"])
 def check_user():
     email = (request.json["email"]).lower()
     password = request.json["password"]
@@ -222,14 +234,16 @@ def check_user():
         return {"found": False}, status.USER_NOT_FOUND
 
 
-@app.route("/api/v1/add_user", methods=["POST"])
+@app.route("/api/v1/add-user", methods=["POST"])
 def add_user():
     doc = {
         "first_name": request.json.get("first_name"),
         "last_name": request.json.get("last_name"),
         "username": request.json.get("username"),
         "email": request.json.get("email"),
-        "password": request.json.get("password")
+        "password": flask_bcrypt.generate_password_hash(
+            request.json.get("password")
+        ).decode(),
     }
     if mongo.db.users.find_one({"email": doc["email"]}) is not None:
         return (
@@ -247,7 +261,7 @@ def add_user():
     return {"success": True, "already": None}
 
 
-@app.route("/api/v1/add_comment", methods=["POST"])
+@app.route("/api/v1/add-comment", methods=["POST"])
 def add_comment():
     blog = request.json["blog_title"]
     comment_type = request.json["type"]
@@ -258,44 +272,35 @@ def add_comment():
 
     if comment_type == "main":
         _id = str(
-            mongo.db.comments.insert_one({
-                "comment": comment_content,
-                "user": request.json["user"]
-            }).inserted_id
+            mongo.db.comments.insert_one(
+                {"comment": comment_content, "user": request.json["user"]}
+            ).inserted_id
         )
         comments_tmp = blog_found["comments"]
-        comments_tmp.append([str(_id), []])
+        comments_tmp.append([_id, []])
     else:
         id_of_comment = request.json["id"]
-        if all(
-            comment[0] != id_of_comment
-            for comment in blog_found["comments"]
-        ):
+        if all(comment[0] != id_of_comment for comment in blog_found["comments"]):
             return {"worked": False}
         _id = str(
-            mongo.db.comments.insert_one({
-                "comment": comment_content,
-                "user": request.json["user"]
-            }).inserted_id
+            mongo.db.comments.insert_one(
+                {"comment": comment_content, "user": request.json["user"]}
+            ).inserted_id
         )
         comments_tmp = blog_found["comments"]
         for index, comment in enumerate(comments_tmp):
             if comment[0] == request.json["id"]:
                 comments_tmp[index][1].append(_id)
                 break
-    mongo.db.blogs.update_one(
-        {"title": blog},
-        {"$set": {"comments": comments_tmp}}
-    )
+    mongo.db.blogs.update_one({"title": blog}, {"$set": {"comments": comments_tmp}})
     return {"worked": True}
 
 
-@app.route("/api/v1/get_blog_comments")
-def get_comments():
-    blog_title = request.args["blog_title"]
+@app.route("/api/v1/blog-comments/<blog_title>")
+def get_comments(blog_title):
     blog = mongo.db.blogs.find_one({"title": blog_title})
     if blog is None:
-        return {"found": False}, status.USER_NOT_FOUND
+        return {"found": False}, status.RESOURCE_NOT_FOUND
     comments = blog["comments"]
     comment_tree = []
     for comment in comments:
@@ -304,15 +309,19 @@ def get_comments():
             "text": comment_data["comment"],
             "user": comment_data["user"],
             "id": str(comment_data["_id"]),
-            "sub_comments": []
+            "sub_comments": [],
         }
         for sub_comment in comment[1]:
-            sub_comment_data = mongo.db.comments.find_one({"_id": ObjectId(str(sub_comment))})
-            all_comments["sub_comments"].append({
-                "text": sub_comment_data["comment"],
-                "user": sub_comment_data["user"],
-                "id": str(sub_comment)
-            })
+            sub_comment_data = mongo.db.comments.find_one(
+                {"_id": ObjectId(str(sub_comment))}
+            )
+            all_comments["sub_comments"].append(
+                {
+                    "text": sub_comment_data["comment"],
+                    "user": sub_comment_data["user"],
+                    "id": str(sub_comment),
+                }
+            )
         comment_tree.append(all_comments)
     return jsonify(comment_tree)
 
@@ -324,17 +333,23 @@ def error_handling(error):
         "error.html",
         error=error,
         code=error.code,
-        login_status=session["logged_in"] if logged_in(session) else None
+        login_status=session["logged_in"] if logged_in(session) else None,
     )
 
 
 @app.context_processor
 def get_blogs():
     def find_blogs():
-        return reversed(sorted(
-            list(mongo.db.blogs.find()),
-            key=lambda date: datetime.datetime.strptime(date["date_released"] + date["time_released"], "%m/%d/20%y%H:%M:%S:%f")
-        ))
+        return reversed(
+            sorted(
+                list(mongo.db.blogs.find()),
+                key=lambda date: datetime.datetime.strptime(
+                    date["date_released"] + date["time_released"],
+                    "%m/%d/20%y%H:%M:%S:%f",
+                ),
+            )
+        )
+
     return dict(find_blogs=find_blogs)
 
 
@@ -342,6 +357,7 @@ def get_blogs():
 def convert_string_to_json():
     def str_to_json(text):
         return {"text": text}
+
     return dict(str_to_json=str_to_json)
 
 
